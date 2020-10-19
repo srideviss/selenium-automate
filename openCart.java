d80022515150aebcb7f0cfb6ee23e4dc894dcb2b
srideviss <72385207+srideviss@users.noreply.github.com>
2020-10-19 14:15:34 +0530

    Update openCart.java

--- openCart.java
+++ openCart.java
@@ -10,7 +10,8 @@ import java.util.concurrent.TimeUnit;
 
 public class openCart {
     public static void main(String[] args)  {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\srideviss\\Downloads\\chromedriver_win32\\chromedriver.exe");
+       // System.setProperty("webdriver.chrome.driver", "C:\\Users\\srideviss\\Downloads\\chromedriver_win32\\chromedriver.exe");
+         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait=new WebDriverWait(driver, 10);;
         driver.get("https://www.opencart.com/index.php?route=account/login");
